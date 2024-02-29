aa82d49419e9a4a48dc3112cc969fc840d9d1ca6
Vitaliy <120054505+VitaliyOne@users.noreply.github.com>
2024-02-29 23:13:32 +0300

    Initialize project

---
+++ .eslintrc.cjs
@@ -0,0 +1,120 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:prettier/recommended'
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts', '.tsx'],
+        moduleDirectory: ['src', 'node_modules']
+      }
+    }
+  },
+  rules: {
+    // @typescript-eslint
+    '@typescript-eslint/no-var-requires': 1,
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports'
+      }
+    ],
+    '@typescript-eslint/no-misused-promises': 'off',
+    // import
+    'import/no-cycle': 'off',
+    'import/no-anonymous-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': ['error', 'never', { css: 'always', json: 'always' }],
+    'import/order': [
+      'error',
+      {
+        groups: ['external', 'index', 'sibling', 'parent', 'builtin', 'object', 'type', 'internal']
+      }
+    ],
+    // prettier
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'none',
+        jsxBracketSameLine: false,
+        endOfLine: 'lf',
+        jsxSingleQuote: false,
+        printWidth: 100,
+        arrowParens: 'always',
+        tabWidth: 2
+      }
+    ],
+    // react
+    'react/jsx-filename-extension': [1, { extensions: ['.js', 'jsx', '.tsx'] }],
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-sort-props': [
+      1,
+      {
+        callbacksLast: true,
+        shorthandFirst: false,
+        shorthandLast: true,
+        multiline: 'last',
+        ignoreCase: true,
+        noSortAlphabetically: false,
+        reservedFirst: true
+      }
+    ],
+    'react/prefer-stateless-function': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'react/require-default-props': 'off',
+    'react/display-name': 'off',
+    // react hook rules
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    // common
+    '@typescript-eslint/no-empty-function': 'off',
+    'comma-dangle': [
+      'error',
+      {
+        arrays: 'never',
+        objects: 'never',
+        imports: 'never',
+        exports: 'never',
+        functions: 'never'
+      }
+    ],
+    'no-shadow': 'off',
+    camelcase: 'off',
+    'no-unused-vars': 'off',
+    'max-classes-per-file': 'off',
+    'no-param-reassign': 'off',
+    'func-names': 'off'
+  }
+};
+
+module.exports = config;
