cb9d6ed8f96853b3d102374e9a63c96d8b612899
Vitaliy <120054505+VitaliyOne@users.noreply.github.com>
2024-03-03 13:37:01 +0300

    Add no tasks placeholder

--- src/components/TaskTrackerCard/TaskTrackerCard.tsx
+++ src/components/TaskTrackerCard/TaskTrackerCard.tsx
@@ -1,13 +1,16 @@
-import { Box, Card } from '@mui/material';
+import { Box, Card, Typography } from '@mui/material';
 import React from 'react';
 import Header from '../Header';
 import TaskList from '../TaskList';
+import useAppSelector from '../../hooks/useAppSelector';
 
 const TaskTrackerCard = () => {
+  const { tasks } = useAppSelector((state) => state.tasks);
+
   return (
     <Box component={Card} display="flex" flexDirection="column" gap={2} p={2} variant="outlined">
       <Header />
-      <TaskList />
+      {tasks.length ? <TaskList /> : <Typography align="center">Задач нет</Typography>}
     </Box>
   );
 };
